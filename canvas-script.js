1514f6a944c1f9b3e006bd851c4d0402d56233f4
IoMyu <132834449+Myucod3@users.noreply.github.com>
2026-02-11 21:39:27 +0100

    Add files via upload

--- canvas-script.js
+++ canvas-script.js
@@ -1,7 +1,21 @@
 const canvas = document.getElementById('post');
 const ctx = canvas.getContext('2d');
 
+const plus = document.getElementById('plus-btn');
+const minus = document.getElementById('mns-btn');
+
 let drawing = false;
+let brushSize = 1;
+
+plus.addEventListener("click", function() {
+    brushSize += 0.4;
+    console.log(brushSize)
+});
+
+minus.addEventListener("click", function() {
+    brushSize -= 0.4;
+    console.log(brushSize)
+});
 
 canvas.addEventListener("pointerdown", e => {
     drawing = true;
@@ -13,9 +27,12 @@ canvas.addEventListener("pointerdown", e => {
 canvas.addEventListener("pointermove", e => {
     if(!drawing) return;
 
+    ctx.lineWidth = brushSize;
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
 });
 
 canvas.addEventListener("pointerup", () => drawing = false);
 canvas.addEventListener("pointerleave", () => drawing = false);
+
+console.log(brushSize)
