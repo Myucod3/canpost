f1d7fab50a37f5fb75d3a6d835c089f7e4acdeaa
IoMyu <132834449+Myucod3@users.noreply.github.com>
2026-03-22 15:06:15 +0100

    Add files via upload

--- posts-script.js
+++ posts-script.js
@@ -2,13 +2,13 @@ document.getElementById('upl-form').addEventListener("submit", e => {
     e.preventDefault();
 
     savePosts();
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
 function savePosts(){
     const imgData = canvas.toDataURL('image/png');
-    document.getElementById('upl-pg').innerHTML = '<h3>Uploads</h3>';
-    
+
     const posts = JSON.parse(localStorage.getItem('posts') || '[]');
     posts.push(imgData);
 
@@ -21,11 +21,13 @@ function loadPosts(){
     post.forEach(item => {
         const img = document.createElement('img');
         img.src = item;
-        img.id = "uploaded-post"
+        img.id = "canvas-post"
         document.getElementById('upl-pg').appendChild(img);
     })
 }
 
-window.onload = function(){
+window.onload = function(e){
+    e.preventDefault();
+    
     loadPosts();
 }
